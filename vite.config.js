d10bd2ef4e3ab521d52822bc8d2b2a6be679b1d6
Ville Säävuori <ville@unessa.net>
2021-01-28 15:48:38 +0200

    fix: fixed alias config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   alias: {
-    '/@/': path.resolve(__dirname, './src'),
+    '@': path.resolve(__dirname, './src'),
   },
 })
