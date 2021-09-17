f7158ddf1b0f6de8839a82a7d5855952fceb54f5
Ville Säävuori <ville@unessa.net>
2021-09-17 15:41:10 +0300

    removed volar plugin

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,18 @@ import * as path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import pkg from './package.json'
-import * as volar from '@volar/experimental/compiler'
 
 process.env.VITE_APP_BUILD_EPOCH = new Date().getTime()
 process.env.VITE_APP_VERSION = pkg.version
 
 export default defineConfig({
-  plugins: [vue(volar.getVuePluginOptionsForVite())],
+  plugins: [
+    vue({
+      script: {
+        refSugar: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
