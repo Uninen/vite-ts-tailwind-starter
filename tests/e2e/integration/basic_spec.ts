7674c18f0c64f2d86b4da54713bcecd0aeb5c41c
Ville Säävuori <ville@unessa.net>
2021-12-13 07:38:16 +0200

    fix: fixed horizontal scrolling, added tests for it

--- tests/e2e/integration/basic_spec.ts
+++ tests/e2e/integration/basic_spec.ts
@@ -6,4 +6,22 @@ describe('Homepage', () => {
 
     cy.contains('h2', 'Hello World Component')
   })
+
+  it('Should not have vertical scroll bars on mobile or desktop', () => {
+    cy.viewport('iphone-5')
+    cy.window().then(() => {
+      const htmlScrollWidth = Cypress.$('html')[0].scrollWidth
+      const htmlWidth = Cypress.$('html')[0].clientWidth
+      const scrollBarWidth = htmlScrollWidth - htmlWidth
+      expect(scrollBarWidth).to.be.eq(0)
+    })
+
+    cy.viewport('macbook-11')
+    cy.window().then(() => {
+      const htmlScrollWidth = Cypress.$('html')[0].scrollWidth
+      const htmlWidth = Cypress.$('html')[0].clientWidth
+      const scrollBarWidth = htmlScrollWidth - htmlWidth
+      expect(scrollBarWidth).to.be.eq(0)
+    })
+  })
 })
