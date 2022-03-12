1aae37b249078775a6505ef9b803cd10d240a8c0
Ville Säävuori <ville@unessa.net>
2022-03-12 15:41:27 +0200

    fix: fix issue w/ Cypress and GH actions

--- tests/component/HelloWorld.spec.ts
+++ tests/component/HelloWorld.spec.ts
@@ -3,24 +3,32 @@ import { createTestingPinia } from '@pinia/testing'
 
 import HelloWorld from '../../src/components/HelloWorld.vue'
 
-it('Passes msg prop correctly', () => {
-  mount(HelloWorld, {
-    propsData: {
-      msg: 'Hello World',
-    },
-    global: {
-      plugins: [
-        createTestingPinia({
-          createSpy: (args) => {
-            console.log('spy', args)
-            return () => {
-              console.log('spyreturns')
-            }
-          },
-        }),
-      ],
-    },
+describe('Basic demo', () => {
+  before(() => {
+    setTimeout(() => {
+      console.log('waiting for CI to catch up')
+    }, 2000)
   })
 
-  cy.get('h2').should('contain.text', 'Hello World')
+  it('Passes msg prop correctly', () => {
+    mount(HelloWorld, {
+      propsData: {
+        msg: 'Hello World',
+      },
+      global: {
+        plugins: [
+          createTestingPinia({
+            createSpy: (args) => {
+              console.log('spy', args)
+              return () => {
+                console.log('spyreturns')
+              }
+            },
+          }),
+        ],
+      },
+    })
+
+    cy.get('h2').should('contain.text', 'Hello World')
+  })
 })
--- tests/e2e/plugins/index.ts
+++ tests/e2e/plugins/index.ts
@@ -4,8 +4,8 @@ export default function (
   on: Cypress.PluginEvents,
   config: Cypress.PluginConfigOptions
 ): void | Cypress.ConfigOptions | Promise<Cypress.ConfigOptions> {
-  on('dev-server:start', async (options: Cypress.DevServerConfig) =>
-    startDevServer({ options })
-  )
+  on('dev-server:start', async (options: Cypress.DevServerConfig) => {
+    return startDevServer({ options })
+  })
   return config
 }
