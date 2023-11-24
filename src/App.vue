6a92c1cf321a25197cc936fa826a0c3ae41e7276
Ville Säävuori <ville@unessa.net>
2023-11-24 18:35:49 +0200

    fix: made head meta reactive

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
+<script setup lang="ts">
+// See vite.config.ts for details about automatic imports
+const route = useRoute()
+
+useHead({
+  title: () => route.meta.title || 'Vite + Vue Template',
+  meta: [
+    {
+      property: 'og:title',
+      content: () => route.meta.title,
+    },
+    {
+      name: 'twitter:title',
+      content: () => route.meta.title,
+    },
+  ],
+})
+</script>
 <template>
   <router-view></router-view>
 </template>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,21 +1,4 @@
 <script setup lang="ts">
-// useRoute, useHead, and HelloWorld are automatically imported. See vite.config.ts for details.
-const route = useRoute()
-
-useHead({
-  title: route.meta.title,
-  meta: [
-    {
-      property: 'og:title',
-      content: route.meta.title,
-    },
-    {
-      name: 'twitter:title',
-      content: route.meta.title,
-    },
-  ],
-})
-
 const VERSION = import.meta.env.VITE_APP_VERSION
 const BUILD_DATE = import.meta.env.VITE_APP_BUILD_EPOCH
   ? new Date(Number(import.meta.env.VITE_APP_BUILD_EPOCH))
