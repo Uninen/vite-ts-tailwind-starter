79bd09b9589bef0c0d332b5cde4c7d65a1a1c3ec
Ville Säävuori <ville@unessa.net>
2023-12-29 14:11:56 +0200

    fix: fix vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -18,15 +18,15 @@ export default mergeConfig(
       include: ['tests/unit/**/*.test.ts', 'src/**/*.spec.ts'],
       root: fileURLToPath(new URL('./', import.meta.url)),
       setupFiles: ['./tests/setup/testglobals.ts'],
-      coverage: {
-        // exclude: ['__mocks__/*', 'tests/*', '**/*.spec.ts'],
-        provider: 'v8',
-        reporter: ['text', 'json', 'json-summary'],
-        lines: 75,
-        branches: 65,
-        functions: 0,
-        statements: 65,
-      },
+      // coverage: {
+      //   // exclude: ['__mocks__/*', 'tests/*', '**/*.spec.ts'],
+      //   provider: 'v8',
+      //   reporter: ['text', 'json', 'json-summary'],
+      //   lines: 75,
+      //   branches: 65,
+      //   functions: 0,
+      //   statements: 65,
+      // },
     },
   })
 )
