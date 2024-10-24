f6c89c9b6992a91542b20203d7af7c160c578a5d
Ville Säävuori <ville@unessa.net>
2024-10-24 22:11:45 +0300

    docs: update homepage

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -8,36 +8,67 @@ const store = useStore()
     <h2>Template Project Features</h2>
 
     <p>
-      The idea of this template is to bundle as many often used and useful features pre-configured as possible. If you
-      don't need something, just remove it!
+      This template is practical and batteries included but not overly complex or bloated. It includes explicit
+      configuration over magic, making it easy to understand how everything works and modify things to your needs.
     </p>
 
     <ul>
       <li>
         Pinia store (fully typed Vuex store available in
-        <a href="https://github.com/Uninen/vite-ts-tailwind-starter/tree/v1.13"> template version 1.13 </a>
-        )
+        <a href="https://github.com/Uninen/vite-ts-tailwind-starter/tree/v1.13">template version 1.13</a>)
       </li>
       <li>
-        Routing using vue-router. See <router-link to="/demo/"> demo page </router-link>. (Also
-        <a href="#" data-testid="pinia-anchor" @click="store.goToDemo"> routable fom the store </a>
-        .)
+        Routing using vue-router. See <router-link to="/demo/">demo page</router-link>. (Also
+        <a href="#" data-testid="pinia-anchor" @click="store.goToDemo">routable from the store</a>.)
       </li>
-      <li>TailwindCSS w/ JIT-mode, purge, plugins, and production minimization pre-configured</li>
-      <li>Vue-friendly Eslint and Prettier configuration - use as is or tweak to your liking</li>
+      <li>TypeScript with fully typed components configured in templates</li>
+      <li>
+        unplugin-vue-components Automatic package and component imports via <code>unplugin-auto-import</code> and
+        <code>unplugin-vue-components</code>
+      </li>
+      <li>
+        Tailwind CSS 3 with following plugins pre-installed:
+        <ul>
+          <li>@tailwindcss/aspect-ratio</li>
+          <li>@tailwindcss/typography</li>
+          <li>@tailwindcss/forms</li>
+          <li>@egoist/tailwindcss-icons</li>
+        </ul>
+      </li>
+      <li>
+        <strong>PostCSS 8</strong> with <code>postcss-nesting</code> plugin and <code>cssnano</code> for minimizing
+        production CSS
+      </li>
+      <li><strong>Eslint 9</strong> with flat config</li>
+      <li>Prettier and EditorConfig</li>
       <li>Alias <code>@</code> to <code>project_root/src</code></li>
-      <li>Predefined and typed global variables</li>
-      <li>Playwright e2e and component testswith coverage preconfigured</li>
-      <li>Github Workflows preconfigured to run e2e and component tests automatically on every push</li>
+      <li>Predefined and fully typed global variables (VITE_APP_VERSION and VITE_APP_BUILD_EPOCH)</li>
+      <li>
+        Comprehensive testing setup:
+        <ul>
+          <li>Vitest for unit and component testing with code coverage</li>
+          <li>Playwright for E2E testing</li>
+        </ul>
+      </li>
+      <li>
+        GitHub workflows for:
+        <ul>
+          <li>Renovatebot for dependency updates</li>
+          <li>Automated unit + component tests</li>
+          <li>Automated e2e tests</li>
+          <li>Coverage reporting in PR comments</li>
+        </ul>
+      </li>
+      <li>Vercel deployment configuration included</li>
     </ul>
 
     <h2>Project setup and usage</h2>
 
     <p>
       Using
-      <a href="https://github.com/johnsoncodehk/volar">Volar extension for VSCode</a>
-      is recommended to take full advance of the new script setup sugar and full TypeScript support in Vue SFC
-      templates. (This is totally optional but recommended as it results much better DX!) See
+      <a href="https://marketplace.visualstudio.com/items?itemName=Vue.volar">Vue extension for VSCode</a>
+      is recommended to take full advantage of the new script setup sugar and full TypeScript support in Vue SFC
+      templates. See
       <a href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support">official IDE Support documentation</a>
       for more details.
     </p>
@@ -78,7 +109,7 @@ const store = useStore()
 
     <p>
       See <code>vite.config.js</code> and <code>src/env.d.ts</code> for the configuration and
-      <code>src/pages/BaseTemplate.vue</code> for usage example.
+      <code>src/App.vue</code> for usage example.
     </p>
 
     <h3>Code Coverage</h3>
@@ -99,7 +130,7 @@ const store = useStore()
     <ul>
       <li>Follow <a href="https://twitter.com/uninen">@Uninen on Twitter</a></li>
       <li>
-        Lots of continuously updating learnings from Vite / Vue / TypeScript and other Web development topics on my
+        Read continuously updating learnings from Vite / Vue / TypeScript and other Web development topics on my
         <a href="https://til.unessa.net/">Today I Learned</a> site
       </li>
     </ul>
@@ -107,8 +138,8 @@ const store = useStore()
     <h2>Contributing</h2>
 
     <p>
-      Contributions are welcome! Please follow
-      <a href="https://www.contributor-covenant.org/version/2/0/code_of_conduct/"> the code of conduct </a>
+      Contributions are welcome! Please follow the
+      <a href="https://www.contributor-covenant.org/version/2/0/code_of_conduct/">code of conduct</a>
       when interacting with others.
     </p>
   </div>
