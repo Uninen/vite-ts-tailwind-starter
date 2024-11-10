2999fd5f92689dbdc7ca86757990005de826bf64
Ville Säävuori <ville@unessa.net>
2024-11-10 17:18:38 +0200

    fix: e2e tests

--- tests/e2e/basic.spec.ts
+++ tests/e2e/basic.spec.ts
@@ -8,16 +8,3 @@ test('homepage details', async ({ page }) => {
   await expect(page).toHaveTitle(defaultTitle)
   await expect(page.locator('h2').first()).toHaveText('Hello World Component')
 })
-
-test('router from pinia', async ({ page }) => {
-  await page.goto('/')
-  await page.getByTestId('pinia-anchor').click()
-  expect(page.url()).toBe('http://localhost:5173/demo/')
-})
-
-test('title changes', async ({ page }) => {
-  await page.goto('/demo/')
-  await expect(page).toHaveTitle('Demo title')
-  await page.goto('/')
-  await expect(page).toHaveTitle(defaultTitle)
-})
